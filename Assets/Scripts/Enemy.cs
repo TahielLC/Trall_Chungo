f667d4057e361ca88f16b1960f905fd2a9944ddb
TahielLC <sentey.44@gmail.com>
2023-08-10 20:11:27 -0300

    funciona el pool

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,14 @@ public class Enemy : RecyclableObject
     public int maxHealt = 100;
     int currentHealt;
     public Vector3 pos;
+    public float slowDistance;
+    Vector2 velocity = Vector2.zero;
+    private SpriteRenderer spEnemy;
+
+    [SerializeField] Transform player;
+    [SerializeField] float followSpeed;
+
+
 
     internal override void Init()
     {
@@ -20,6 +28,7 @@ public class Enemy : RecyclableObject
     }
     private void Start()
     {
+        spEnemy = GetComponent<SpriteRenderer>();
         pos = transform.position;
         pos += new Vector3(2, 2, 0);
         currentHealt = maxHealt;
@@ -27,6 +36,29 @@ public class Enemy : RecyclableObject
 
     }
 
+    private void Steering()
+    {
+        Vector2 targetPosition = player.position;
+        Vector2 playerDistance = (targetPosition - (Vector2)transform.position);
+        Vector2 disiredVelocity = playerDistance.normalized * followSpeed;
+        Vector2 steering = disiredVelocity - velocity;
+
+        velocity += steering * Time.deltaTime;
+
+        float slowDownFactor = Mathf.Clamp01(playerDistance.magnitude / slowDistance);
+
+        velocity *= slowDownFactor;
+
+        transform.position += (Vector3)velocity * Time.deltaTime;
+
+        spEnemy.flipX = velocity.x < 0;
+        if ((velocity.x < 0))
+        {
+
+        }
+        //attackPonit.eulerAngles = new Vector3(player.position.x, player.position.y, 0);
+
+    }
 
     public void DanioRecibido(int hit)
     {
